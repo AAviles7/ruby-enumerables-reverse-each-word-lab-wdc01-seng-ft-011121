cd7e48e10f7f325a4bb9c1a035d6d534ac87c748
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-04 05:04:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,15 +5,6 @@ def reverse_each_word(string)
   s_array.collect{|s| s.reverse!}
   return s_array.join(" ")
 #=======
-  
-  s = string.split(" ")
-  s_array = Array.new
-  s_array.push(s)
-  
-  #s_array.each{|s| s.reverse!}
-  ans = s_array[0][0]
-  
-  return ans
 #>>>>>>> 6c30b32f9cca74e66483dc69193debf8d81116b7
   
 end
