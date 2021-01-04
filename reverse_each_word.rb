dd5bddf673d2b5637dd4c0761ec0332d556e568b
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-04 05:04:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,5 @@
 def reverse_each_word(string)
-  
-#<<<<<<< HEAD
   s_array = string.split(" ")
   s_array.collect{|s| s.reverse!}
   return s_array.join(" ")
-#=======
-#>>>>>>> 6c30b32f9cca74e66483dc69193debf8d81116b7
-  
 end
